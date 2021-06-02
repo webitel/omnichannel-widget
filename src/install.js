56a2f35bf18b5f559a4c314a0dce8fd2f47cd0dd
d.lohvinov <d.lohvinov@webitel.com>
2021-06-02 13:34:41 +0300

    init commit

---
+++ src/install.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+const Instance = new Vue({
+  store,
+  data: () => ({ config: {} }),
+  render: (h) => h(App),
+});
+
+export default class SiteButton {
+  constructor(selector, config) {
+    Vue.set(Instance, 'config', {
+      ...config,
+    });
+    Instance.$mount(selector);
+    return Instance;
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import SiteButton from './install';
+
+Vue.config.productionTip = false;
+
+export default new SiteButton('#app', {});
