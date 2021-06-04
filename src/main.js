7bc9e1362b3c479d404e6612e63a4d15e4969d0a
dlohvinov <nsblnr@gmail.com>
2021-06-04 17:58:34 +0300

    dev: added colors, shadow css variables [WTEL-1908]

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import WtOmniWidget from './install';
+import './css/_main.scss';
 
 // install workers
-import './workers/websocket-shared-worker/install';
+// import './workers/websocket-shared-worker/install';
 
 Vue.config.productionTip = false;
 
