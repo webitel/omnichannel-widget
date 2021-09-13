4ba5b41b25fe1e4c668c6fe24c4c2189a97a9724
dlohvinov <nsblnr@gmail.com>
2021-09-13 20:51:41 +0300

    feature: added message link parser [WTEL-2033]

--- src/modules/chat/components/wt-omni-widget-chat-content/messages/wt-omni-widget-chat-text-message.vue
+++ src/modules/chat/components/wt-omni-widget-chat-content/messages/wt-omni-widget-chat-text-message.vue
@@ -2,17 +2,24 @@
   <div
     class="wt-omni-widget-chat-message wt-omni-widget-chat-message--text"
     :class="{ 'wt-omni-widget-chat-message--my': my }"
-  >
-    {{ message.text }}
-  </div>
+    v-html="text"
+  ></div>
 </template>
 
 <script>
+import linkifyHtml from 'linkifyjs/html';
 import chatMessageMixin from '../../../mixins/chatMessageMixin';
 
 export default {
   name: 'wt-omni-widget-chat-text-message',
   mixins: [chatMessageMixin],
+  computed: {
+    text() {
+      return linkifyHtml(this.message.text, {
+        target: '_blank',
+      });
+    },
+  },
 };
 </script>
 
