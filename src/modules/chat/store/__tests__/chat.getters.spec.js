12a5693963ac0a8487d7d2a07ea14ed68a0d83a3
dlohvinov <nsblnr@gmail.com>
2022-06-27 20:30:13 +0300

    feature: added message btns hiding if new "my" message is sent [WTEL-2655]

--- src/modules/chat/store/__tests__/chat.getters.spec.js
+++ src/modules/chat/store/__tests__/chat.getters.spec.js
@@ -15,4 +15,19 @@ describe('chat module: getters', () => {
 
     expect(chat.getters.IS_MY_MESSAGE(state)(message)).toBe(false);
   });
+
+  const myMsg = { id: 1 };
+  const btnsMsg1 = { id: 2 };
+  const btnsMsg2 = { id: 3 };
+  const IS_MY_MESSAGE = (msg) => msg === myMsg;
+  const getters = { IS_MY_MESSAGE };
+  it('SHOW_BUTTONS correctly computes no-buttons if my message is after buttons', () => {
+    const state = { messages: [btnsMsg1, myMsg] };
+    expect(chat.getters.SHOW_BUTTONS(state, getters)(btnsMsg1)).toBe(false);
+  });
+  it('SHOW_BUTTONS correctly computes buttons on two messages if no next myMsg', () => {
+    const state = { messages: [btnsMsg1, btnsMsg2] };
+    expect(chat.getters.SHOW_BUTTONS(state, getters)(btnsMsg1)).toBe(true);
+    expect(chat.getters.SHOW_BUTTONS(state, getters)(btnsMsg2)).toBe(true);
+  });
 });
