b5f771a9c94fb416cb086db4637bb8da7a37b30b
dlohvinov <nsblnr@gmail.com>
2022-12-14 19:32:20 +0200

    refactor: appointments post and delete methods refactored to get [WTEL-3136]

--- src/modules/appointment/api/appointment.js
+++ src/modules/appointment/api/appointment.js
@@ -1,8 +1,29 @@
+import qs from 'qs';
 import instance from '../../../app/api/instance.axios';
 
 const getAppointment = async (url) => instance.get(url);
-const postAppointment = async (url, form) => instance.post(url, form);
-const deleteAppointment = async (url) => instance.delete(url);
+const postAppointment = async (baseUrl, {
+  destination,
+  name,
+  scheduleDate: date,
+  scheduleTime: time,
+  variables,
+}) => {
+  const data = {
+    destination,
+    name,
+    date,
+    time,
+    email: variables.email,
+    massage: variables.message,
+  };
+  const url = baseUrl.concat(`/create?${qs.stringify(data)}`);
+  return instance.get(url);
+};
+const deleteAppointment = async (baseUrl) => {
+  const url = baseUrl.concat('/cancel');
+  return instance.delete(url);
+};
 
 const AppointmentAPI = {
   getAppointment,
