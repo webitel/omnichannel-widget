9ad81235574af42cb01f9cf8e3956fbee5e9ed72
dlohvinov <nsblnr@gmail.com>
2022-12-15 19:48:38 +0200

    refactor: refactored api to localStorage cookies [WTEL-3136]

--- src/modules/appointment/api/appointment.js
+++ src/modules/appointment/api/appointment.js
@@ -1,8 +1,38 @@
 import instance from '../../../app/api/instance.axios';
 
-const getAppointment = async (url) => instance.get(url);
-const postAppointment = async (url, form) => instance.post(url, form);
-const deleteAppointment = async (url) => instance.delete(url);
+const setKey = (key) => localStorage.setItem('wt-appoinment-key', key);
+const getKey = () => localStorage.getItem('wt-appoinment-key');
+
+const setExpire = (value) => localStorage.setItem('wt-appointment-key-expire', value);
+const getExpire = () => localStorage.getItem('wt-appointment-key-expire');
+
+const clearKey = () => {
+  localStorage.removeItem('wt-appoinment-key');
+  localStorage.removeItem('wt-appoinment-key-expire');
+};
+
+const checkKey = () => {
+  if (getExpire() <= Date.now()) clearKey();
+};
+
+const generateHeaders = () => {
+  checkKey();
+  const key = getKey();
+  return key ? { 'X-WBT-KEY': key } : {};
+};
+
+const getAppointment = async (url) => instance.get(url, { headers: generateHeaders() });
+const postAppointment = async (url, form) => {
+  const response = await instance.post(url, form);
+  const { key, expireKey } = response.appointment;
+  setKey(key);
+  setExpire(expireKey);
+  return response;
+};
+const deleteAppointment = async (url) => {
+  await instance.delete(url, { headers: generateHeaders() });
+  clearKey();
+};
 
 const AppointmentAPI = {
   getAppointment,
